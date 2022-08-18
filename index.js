c9d5a42151dc6196a5c2c8b1c02e06b1fffd568c
first-name second-name <silvia.wachira@student.moringa.com>
2022-08-18 23:06:30 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Code your solution in this file!
+
+const scuberHq = 42;
+
+const blockDistance = 264;
+
+function distanceFromHqInBlocks(pickUpLocation) {
+    let blockDifference;
+    if (pickUpLocation > scuberHq) {
+        blockDifference = pickUpLocation - scuberHq;
+    } else {
+        blockDifference = scuberHq - pickUpLocation;
+    }
+    return blockDifference;
+}
+function distanceFromHqInFeet(pickUpLocation) {
+    let feetDistance = distanceFromHqInBlocks(pickUpLocation)
+    return feetDistance * blockDistance;
+    
+}
+
+function distanceTravelledInFeet(pickUpLocation,scuberHq) {
+    let distanceTravelled;
+    if (pickUpLocation > scuberHq) {
+        distanceTravelled = (pickUpLocation - scuberHq) * blockDistance;
+    } else {
+        distanceTravelled = (scuberHq - pickUpLocation) * blockDistance;
+    }
+    return distanceTravelled;
+}
+
+function calculatesFarePrice(pickUpLocation,scuberHq) {
+    let distanceFare = distanceTravelledInFeet(pickUpLocation,scuberHq);
+    let fare;
+    if (distanceFare > 0 && distanceFare <= 400) {
+        fare = 0;
+    } else if (distanceFare > 400 && distanceFare <= 2000) {
+        fare = (distanceFare - 400) * 0.02;
+    } else if (distanceFare > 2000 && distanceFare <= 2500) {
+        fare = 25;
+    } else {
+        return 'cannot travel that far'
+    }
+    return fare;
+    
+}
+
+
+
+
+
+
